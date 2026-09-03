6da483eac3533d81b2265c62c0b67b2b45158268
bruno guedes <brunopassamonti@gmail.com>
2026-09-03 19:53:44 -0300

    Add audited portal data and territory profiles

---
+++ data.js
@@ -0,0 +1,683 @@
+window.PORTAL_DATA = {
+  "tapData": {
+    "Jerry Whilem": [
+      {
+        "name": "Baiuca DJ Bar - Santo Cristo",
+        "area": "Santo Cristo",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "Cervejaria Madre de Capotá",
+        "area": "Botafogo",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "Circo Voador",
+        "area": "Lapa",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "Macuna",
+        "area": "Botafogo",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "Pink Flamingo - Copacabana",
+        "area": "Copacabana",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "Trauma bar - centro",
+        "area": "Centro",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "Vuvu - Botafogo",
+        "area": "Área não informada",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      }
+    ],
+    "Julia Gutvilen": [],
+    "João Pedro Marques": [
+      {
+        "name": "AMATA",
+        "area": "São Paulo",
+        "type": "3 taps simples",
+        "units": 3,
+        "capacity": 3
+      },
+      {
+        "name": "Baixo Pinheiros",
+        "area": "Pinheiros",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "BALLFIVE",
+        "area": "CONSOLACAO",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "Baron Rock Bar",
+        "area": "Centro",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "BOTECO AUGUSTA",
+        "area": "CENTRO",
+        "type": "2 taps triplas",
+        "units": 2,
+        "capacity": 6
+      },
+      {
+        "name": "Boteco do meio",
+        "area": "São Paulo",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "Bottled Dog",
+        "area": "Itaim Bibi",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "CASA DO MEIO",
+        "area": "São Paulo",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "Curtiça",
+        "area": "Vila madalena",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "EPHIGENIA",
+        "area": "São Paulo",
+        "type": "3 taps triplas",
+        "units": 3,
+        "capacity": 9
+      },
+      {
+        "name": "Groove bar",
+        "area": "pinheiros",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "JANELA BAR TATUAPÉ",
+        "area": "São Paulo",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "Janela do meio",
+        "area": "São Paulo",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "MEIO FIO",
+        "area": "São Paulo",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "Pub St. John\\'s Irish",
+        "area": "Tatuapé",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "Santa Augusta",
+        "area": "Augusta",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "Serras Bar",
+        "area": "São Paulo",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "St. Patrick TATUAPÉ",
+        "area": "Tatuapé",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "THE BLUE PUB BOA VISTA",
+        "area": "São Paulo",
+        "type": "Tap simples",
+        "units": 1,
+        "capacity": 1
+      },
+      {
+        "name": "Vila madrugada",
+        "area": "São Paulo",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      }
+    ],
+    "Maria Clarentino": [],
+    "Mani Filardi": [],
+    "Marcelo Martins": [
+      {
+        "name": "Folks RP",
+        "area": "Jardim Sao Luiz",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      },
+      {
+        "name": "Villa Dionisio Ribeirao Preto",
+        "area": "jardim sumare",
+        "type": "Tap dupla",
+        "units": 1,
+        "capacity": 2
+      }
+    ],
+    "Richard Cordeiro": [
+      {
+        "name": "Tatu Bola",
+        "area": "Pituba",
+        "type": "Tap tripla",
+        "units": 1,
+        "capacity": 3
+      }
+    ],
+    "Gustavo Viana": []
+  },
+  "activationRegister": "https://docs.google.com/spreadsheets/d/1DQxE4cigFHxF61TdY180vVIelzeRaw0g5J4ykZ_muxY/edit",
+  "evidenceRoot": "https://drive.google.com/drive/folders/1O_PEMXBluxIDtdTQUs6zbju2f-hH7Ahf",
+  "evidenceFolders": {
+    "Jerry Whilem": "https://drive.google.com/drive/folders/1pjW4F-Cbr7mpC_rKPH6K2BkkHgTFvfBt",
+    "Julia Gutvilen": "https://drive.google.com/drive/folders/1nHQQMe9fIwaP74TrFCyvlaMP3Wo-Eh00",
+    "João Pedro Marques": "https://drive.google.com/drive/folders/13NNc7Goc7pCqRrbFsUe5K6dFL_pL3CRR",
+    "Maria Clarentino": "https://drive.google.com/drive/folders/1jGmoeKIFYrbYbJ4RsCzr6OJM5Om0nJoX",
+    "Mani Filardi": "https://drive.google.com/drive/folders/1vmwyhpfObsBYn6h-WidP85JEzQhbWWXY",
+    "Marcelo Martins": "https://drive.google.com/drive/folders/1BnEUQoGXXqszknVTwmk27Q3X-doIp_4u",
+    "Richard Cordeiro": "https://drive.google.com/drive/folders/1Ow5ekPwYhZSHmNY_zw6zseeiPPBBtu6k",
+    "Gustavo Viana": "https://drive.google.com/drive/folders/1e3ADyiLDRzy3-lHSI5lyNYtYQwrg73UU"
+  },
+  "territoryProfiles": {
+    "TODOS": {
+      "city": "SP + RJ",
+      "areas": [
+        "Rio de Janeiro",
+        "São Paulo",
+        "Ribeirão Preto",
+        "Campinas"
+      ],
+      "hotzones": [
+        "Botafogo",
+        "Lagoa",
+        "Pinheiros",
+        "Vila Madalena",
+        "Centro / República",
+        "Tatuapé",
+        "Cambuí"
+      ],
+      "castles": [
+        "Bosque Bar",
+        "Canastra Rosé"
+      ],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": true,
+          "note": "densidade e frequência"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": true,
+          "note": "imagem e legitimação"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": true,
+          "note": "fluxo e descoberta"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": true,
+          "note": "cultura e influência"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "SP + RJ",
+          "query": "São Paulo e Rio de Janeiro Brasil"
+        }
+      ]
+    },
+    "Jerry Whilem": {
+      "city": "Rio de Janeiro · RJ",
+      "areas": [
+        "Botafogo / Humaitá",
+        "Centro / Lapa",
+        "Lagoa / Gávea",
+        "Copacabana / Ipanema / Leblon"
+      ],
+      "hotzones": [
+        "Botafogo",
+        "Lagoa",
+        "Centro / Lapa",
+        "Ipanema",
+        "Leblon",
+        "Copacabana"
+      ],
+      "castles": [
+        "Bosque Bar",
+        "Canastra Rosé"
+      ],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": true,
+          "note": "Botafogo, Lagoa e Centro"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": true,
+          "note": "Bosque e Canastra Rosé"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": true,
+          "note": "Copa, Ipanema e Leblon"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": true,
+          "note": "cultura e nightlife"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "Botafogo",
+          "query": "Botafogo Rio de Janeiro bares"
+        },
+        {
+          "label": "Lagoa",
+          "query": "Lagoa Rio de Janeiro bares"
+        },
+        {
+          "label": "Centro / Lapa",
+          "query": "Lapa Rio de Janeiro bares"
+        },
+        {
+          "label": "Zona Sul",
+          "query": "Ipanema Leblon Rio de Janeiro bares"
+        }
+      ]
+    },
+    "Julia Gutvilen": {
+      "city": "Rio de Janeiro · RJ",
+      "areas": [
+        "Botafogo",
+        "Barra da Tijuca / Olegário",
+        "Orla"
+      ],
+      "hotzones": [
+        "Botafogo",
+        "Olegário / Barra"
+      ],
+      "castles": [],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": true,
+          "note": "Botafogo e Olegário"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": false,
+          "note": "a mapear"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": true,
+          "note": "Barra e orla"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": true,
+          "note": "contas de influência"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "Botafogo",
+          "query": "Botafogo Rio de Janeiro bares"
+        },
+        {
+          "label": "Olegário / Barra",
+          "query": "Avenida Olegário Maciel Barra da Tijuca bares"
+        }
+      ]
+    },
+    "João Pedro Marques": {
+      "city": "São Paulo · SP",
+      "areas": [
+        "Pinheiros",
+        "Tatuapé",
+        "Barra Funda",
+        "Baixo Augusta",
+        "Centro / República"
+      ],
+      "hotzones": [
+        "Pinheiros",
+        "Barra Funda",
+        "Tatuapé",
+        "Baixo Augusta",
+        "Centro / República"
+      ],
+      "castles": [],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": true,
+          "note": "Pinheiros, Augusta e Tatuapé"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": true,
+          "note": "key accounts da carteira"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": false,
+          "note": "não aplicável"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": true,
+          "note": "nightlife e influência"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "Pinheiros",
+          "query": "Pinheiros São Paulo bares"
+        },
+        {
+          "label": "Tatuapé",
+          "query": "Tatuapé São Paulo bares"
+        },
+        {
+          "label": "Barra Funda",
+          "query": "Barra Funda São Paulo bares"
+        },
+        {
+          "label": "Baixo Augusta",
+          "query": "Baixo Augusta São Paulo bares"
+        }
+      ]
+    },
+    "Maria Clarentino": {
+      "city": "São Paulo · SP",
+      "areas": [
+        "Centro / República",
+        "Bela Vista",
+        "Consolação",
+        "Vila Mariana"
+      ],
+      "hotzones": [
+        "Centro / República",
+        "Baixo Augusta"
+      ],
+      "castles": [],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": true,
+          "note": "Centro e Baixo Augusta"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": true,
+          "note": "key accounts da carteira"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": false,
+          "note": "não aplicável"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": true,
+          "note": "cultura e gastronomia"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "Centro / República",
+          "query": "República São Paulo bares"
+        },
+        {
+          "label": "Baixo Augusta",
+          "query": "Baixo Augusta São Paulo bares"
+        },
+        {
+          "label": "Vila Mariana",
+          "query": "Vila Mariana São Paulo bares"
+        }
+      ]
+    },
+    "Mani Filardi": {
+      "city": "São Paulo · SP",
+      "areas": [
+        "Pinheiros",
+        "Vila Madalena",
+        "ABC Paulista",
+        "Centro"
+      ],
+      "hotzones": [
+        "Pinheiros",
+        "Vila Madalena",
+        "Guaicuí",
+        "Beco do Batman"
+      ],
+      "castles": [],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": true,
+          "note": "Pinheiros e Vila Madalena"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": false,
+          "note": "a mapear"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": false,
+          "note": "não aplicável"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": true,
+          "note": "Beco do Batman e Guaicuí"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "Pinheiros",
+          "query": "Pinheiros São Paulo bares"
+        },
+        {
+          "label": "Vila Madalena",
+          "query": "Vila Madalena São Paulo bares"
+        },
+        {
+          "label": "ABC",
+          "query": "ABC Paulista bares"
+        }
+      ]
+    },
+    "Marcelo Martins": {
+      "city": "Ribeirão Preto · SP",
+      "areas": [
+        "Centro",
+        "Jardim Sumaré",
+        "Campos Elíseos"
+      ],
+      "hotzones": [
+        "Centro de Ribeirão Preto",
+        "Jardim Sumaré"
+      ],
+      "castles": [],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": true,
+          "note": "clusters regionais"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": true,
+          "note": "key accounts da carteira"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": false,
+          "note": "não aplicável"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": true,
+          "note": "cena regional"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "Centro",
+          "query": "Centro Ribeirão Preto bares"
+        },
+        {
+          "label": "Jardim Sumaré",
+          "query": "Jardim Sumaré Ribeirão Preto bares"
+        }
+      ]
+    },
+    "Richard Cordeiro": {
+      "city": "Campinas · SP",
+      "areas": [
+        "Cambuí",
+        "Taquaral",
+        "Jardim Proença"
+      ],
+      "hotzones": [
+        "Cambuí",
+        "Taquaral"
+      ],
+      "castles": [],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": true,
+          "note": "Cambuí e Taquaral"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": false,
+          "note": "a mapear"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": false,
+          "note": "não aplicável"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": true,
+          "note": "cena regional"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "Cambuí",
+          "query": "Cambuí Campinas bares"
+        },
+        {
+          "label": "Taquaral",
+          "query": "Taquaral Campinas bares"
+        }
+      ]
+    },
+    "Gustavo Viana": {
+      "city": "Praça em validação",
+      "areas": [
+        "Área a consolidar"
+      ],
+      "hotzones": [],
+      "castles": [],
+      "pillars": [
+        {
+          "name": "Hotzone",
+          "active": false,
+          "note": "a mapear"
+        },
+        {
+          "name": "Key Account / Castelo",
+          "active": false,
+          "note": "a mapear"
+        },
+        {
+          "name": "Orla / Turismo",
+          "active": false,
+          "note": "a mapear"
+        },
+        {
+          "name": "Cena / Tendência",
+          "active": false,
+          "note": "a mapear"
+        }
+      ],
+      "mapAreas": [
+        {
+          "label": "Brasil",
+          "query": "Brasil"
+        }
+      ]
+    }
+  }
+};
+
